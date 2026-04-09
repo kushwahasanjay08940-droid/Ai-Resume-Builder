2999bc2637d8c1d90375cb539972ab1854bc6d9e
Sanjay Kushwaha <kushwahasanjay08940@gmail.com>
2026-04-09 07:02:57 +0530

    Update api.js

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ai-resume-builder-hfw4.onrender.com/",
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 export default api;
